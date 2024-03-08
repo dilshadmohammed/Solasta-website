9eee7af3555791f3e28c621825212d19f53aeb60
noel <inbox.noel1107@gmail.com>
2024-03-08 21:30:12 +0530

    Notfound cancellation and terms modified

--- src/views/home/cancellation.jsx
+++ src/views/home/cancellation.jsx
@@ -9,34 +9,34 @@ const CancelPage = () => {
     <div className="container max-w-screen-xl mx-auto px-4 py-20">
       <h1 className="text-5xl font-bold mb-8">Cancellation and Refund</h1>
       <h1 className="text-3xl font-bold">Cancellation</h1>
-      <p className="py-2 text-lg">
+      {/* <p className="py-2 text-lg">
         There is no cancellation available for any items unless the minimum number of
         participants requirement has not been fulfilled <br /> Cancellation guidelines
         regarding each category has been given below:
-      </p>
+      </p> */}
       <h2 className="pl-4 text-xl mb-2 mt-4">● Tech Fest:</h2>
-      <p className="pl-10">
+      {/* <p className="pl-10">
         ○ Cancellation for all events will only be processed under the circumstance of
         lack of a minimum 20 participants. <br />○ Cancellation for Expo registration will
         only be processed under the circumstance of lack of a minimum 10 participants.
-      </p>
+      </p> */}
       <h2 className="pl-4 text-xl mb-2 mt-4">● Workshops:</h2>
-      <p className="pl-10">
+      {/* <p className="pl-10">
         ○ Cancellation for Workshops will only be processed under the circumstance of lack
         of a minimum of 20 participants.
-      </p>
+      </p> */}
       <h2 className="pl-4 text-xl mb-2 mt-4">● Culturals:</h2>
       <p className="pl-10">○ Cancellation.</p>
       <h2 className="pl-4 text-xl mb-2 mt-4">● Night Programs:</h2>
-      <p className="pl-10">
+      {/* <p className="pl-10">
         ○ There is no cancellation available for the tickets of night time programs of
         Karma including the DJ and Bad performances.
-      </p>
-      <p className="py-2 text-lg my-8">
+      </p> */}
+      {/* <p className="py-2 text-lg my-8">
         We are not responsible for any errors or misinterpretations on your part.
-      </p>
+      </p> */}
       <h1 className="text-3xl font-bold">Refund</h1>
-      <p className="py-2 text-lg leading-relaxed">
+      {/* <p className="py-2 text-lg leading-relaxed">
         <ul className="list-disc pl-6">
           <li>
             In case the cancellation process has succeeded, the refund amount will be
@@ -51,7 +51,7 @@ const CancelPage = () => {
           </li>
           <li>A refund will not be issued unless the event is canceled.</li>
         </ul>
-      </p>
+      </p> */}
     </div>
   );
 };
