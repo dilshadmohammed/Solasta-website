ed606080d5e9872cd5574673837bfd3a139133fe
noel <inbox.noel1107@gmail.com>
2024-03-09 09:45:14 +0530

    dynamic background added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 import fullBg from "../assets/full-bg.svg"
 import Footer from "../components/Footer/Footer";
 import purlpleBg from "../assets/v882-aew-56-b.jpg"
+import { BackgroundGradientAnimation } from "../components/ui/background-gradient-animation";
 
 const MainLayout = () => {
   return (
@@ -11,15 +12,18 @@ const MainLayout = () => {
       //   background: `url(${fullBg}) no-repeat center center/cover`,
       // }}
     > <div className="fixed inset-0 w-lvw h-lvh -z-10 opacity-30"
-    style={{
-        background: `url(${fullBg}) no-repeat center center/cover`,
-      }}
-      ></div>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+    // style={{
+    //     background: `url(${fullBg}) no-repeat center center/cover`,
+    //   }}   
+      >
+        <BackgroundGradientAnimation containerClassName={"no-repeat center center/cover"}>
+        </BackgroundGradientAnimation>
+      </div>
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
     </div>
   );
 };
