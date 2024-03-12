1c237ef45d9d9e1a09d81e24ec9a0ae78055f46f
athulmenondev <athulmenondev@gmail.com>
2024-03-13 00:31:17 +0530

    added content

--- src/data/events.js
+++ src/data/events.js
@@ -1,40 +1,128 @@
 import comingSoon from "../assets/events/coming-soon.png"
+import hacksphere from "../assets/events/hacksphere.jpg"
+import dedsec from "../assets/events/dedsec.jpg"
+import cluechronicles from "../assets/events/clue-chronicles.jpg"
+import justaminute from "../assets/events/just-a-minute.jpg"
+import cookintwominutes from "../assets/events/cook-in-two-minutes.jpg"
+import bloggingbonzona from "../assets/events/blogging-bonzona.jpg"
+import promptopia from "../assets/events/promptopia.jpg"
+import debate from "../assets/events/debate.jpg"
+import quizquest from "../assets/events/quiz-quest.jpg"
+import wikichase from "../assets/events/wikichase.jpg"
 
 const events = [
   {
-    id: "coming-soon",
-    title: "Coming Soon",
-    dateTime: "22 MARCH 2024, 2 PM - 4 PM",
-    image: comingSoon,
-    image1: comingSoon,
+    id: "hackspere",
+    title: "HACKSPHERE",
+    dateTime: "23 MARCH 2024, 10 AM - 24 MARCH 2024, 10 AM",
+    image: hacksphere,
+    image1: hacksphere,
     description: ["Team of 3 or 4 members","Free registartion"],
-    fee: 0,
-    prizes: ["1st Prize: ₹3000", "2nd Prize: ₹1500","3rd Prize: ₹500"],
-    registerLink: "https:google.com",
+    fee: 200,
+    prizes: ["1st Prize: TBA","2nd Prize: TBA","3rd Prize: TBA"],
+    registerLink: "https://forms.gle/itupEmNuiZPRq5b77",
   },
   {
-    id: "coming-soon",
-    title: "Coming Soon",
-    dateTime: "22 MARCH 2024, 2 PM - 4 PM",
-    image: comingSoon,
-    image1: comingSoon,
+    id: "decsec",
+    title: "DEDSEC",
+    dateTime: "23 MARCH 2024, 9 AM -24 MARCH 9 AM",
+    image: dedsec,
+    image1: dedsec,
     description: ["Team of 3 or 4 members","Free registartion"],
-    fee: 0,
-    prizes: ["1st Prize: ₹3000", "2nd Prize: ₹1500","3rd Prize: ₹500"],
-    registerLink: "https:google.com",
+    fee: 50,
+    prizes: ["1st Prize:TBA", "2nd Prize:TBA","3rd Prize:*TBA"],
+    registerLink: "https://surl.li/rkpsb",
+  },
+
+  {
+    id: "clue-chronicles",
+    title: "CLUE CHRONICLES",
+    dateTime: "19 MARCH 2024, 7 PM- 22 MARCH 2024",
+    image: cluechronicles,
+    image1: cluechronicles,
+    description: ["Team of 3 or 4 members","Free registartion"],
+    fee: 50,
+    prizes: ["1st Prize:TBA", "2nd Prize: TBA","3rd Prize:TBA"],
+    registerLink: "https://forms.gle/aVvKXs5fXfzGcagt7",
   },
   {
-    id: "coming-soon",
-    title: "Coming Soon",
-    dateTime: "22 MARCH 2024, 2 PM - 4 PM",
-    image: comingSoon,
-    image1: comingSoon,
+    id: "Just-a-minute",
+    title: "JAM:JUST A MINUTE",
+    dateTime: "24 MARCH 2024, 2 PM - 3 PM",
+    image: justaminute,
+    image1: justaminute,
     description: ["Team of 3 or 4 members","Free registartion"],
-    fee: 0,
-    prizes: ["1st Prize: ₹3000", "2nd Prize: ₹1500","3rd Prize: ₹500"],
-    registerLink: "https:google.com",
+    fee: 20,
+    prizes: ["1st Prize: TBA", "2nd Prize: TBA","3rd Prize:TBA"],
+    registerLink: "https://forms.gle/QLbDsDjAxuWmXcpP8",
   },
   
+  {
+    id: "cook-in-two-minutes",
+    title: "Cook in two minutes",
+    dateTime: "24 MARCH 2024, 3 PM - 4 PM",
+    image: cookintwominutes,
+    image1: cookintwominutes,
+    description: ["Team of 3 or 4 members","Free registartion"],
+    fee: 20,
+    prizes: ["1st Prize:TBA", "2nd Prize: TBA","3rd Prize: TBA"],
+    registerLink: "https://forms.gle/k3Pt4JBbchvvw9Jw8",
+  },
+  {
+    id: "blogging-bonanza",
+    title: "BLOGGING BONANZA",
+    dateTime: "23 MARCH 2024, 2 PM - 4 PM",
+    image: bloggingbonzona,
+    image1: bloggingbonzona,
+    description: ["Team of 3 or 4 members","Free registartion"],
+    fee: 500,
+    prizes: ["1st Prize:TBA", "2nd Prize:TBA","3rd Prize:TBA"],
+    registerLink: "https://forms.gle/PfLobaHqxceME39G7",
+  },
+  {
+    id: "promtopiya",
+    title: "PROMTOPIA",
+    dateTime: "24 MARCH 2024, 11 AM - 11:30 AM",
+    image: promptopia,
+    image1: promptopia,
+    description: ["Team of 3 or 4 members","Free registartion"],
+    fee: 0,
+    prizes: ["1st Prize: ₹500"],
+    registerLink: "https://forms.gle/bYiTJfj1X5pYxfjm8",
+  },
+  {
+    id: "debate",
+    title: "DEBATE",
+    dateTime: "23 MARCH 2024, 3 PM - 4 PM",
+    image: debate ,
+    image1: debate ,
+    description: ["Team of 3 or 4 members","Free registartion"],
+    fee: 0,
+    prizes: ["1st Prize:TBA", "2nd Prize: TBA","3rd Prize: TBA"],
+    registerLink: "https://forms.gle/2fGRAoWsNbZuHMD27",
+  },
+  {
+    id: "quiz-quest",
+    title: "QUIZ QUEST",
+    dateTime: "22 MARCH 2024, 2:30 PM",
+    image: quizquest,
+    image1: quizquest,
+    description: ["Team of 3 or 4 members","Free registartion"],
+    fee: 30,
+    prizes: ["1st Prize: TBA", "2nd Prize: TBA","3rd Prize: TBA"],
+    registerLink: "https://forms.gle/RvTtBUDnf4UFhPPA8",
+  },
+  {
+    id: "wikichase",
+    title: "WIKICHASE",
+    dateTime: "23 MARCH 2024, 10 AM - 11 AM",
+    image: wikichase,
+    image1: wikichase,
+    description: ["Team of 3 or 4 members","Free registartion"],
+    fee: 20,
+    prizes: ["1st Prize: TBA", "2nd Prize: TBA","3rd Prize: TBA"],
+    registerLink: "https://forms.gle/MioSL4u4rHopxhe39",
+  }
 ];
 
 export default events;
